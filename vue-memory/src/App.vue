f0a03743e43ad273f69d4a6b59b7d13c1aad6786
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-07-22 14:21:06 +0200

    feat: added base structure of project

--- vue-memory/src/App.vue
+++ vue-memory/src/App.vue
@@ -3,10 +3,14 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <h1>Memory</h1>
-  <RouterView />
+    <h1>Memory</h1>
+    <RouterView />
 </template>
 
 <style scoped>
-
+h1{
+  font-weight: normal;
+  margin: 15px;
+  font-size: 5rem;
+}
 </style>
--- vue-memory/src/views/HomeView.vue
+++ vue-memory/src/views/HomeView.vue
@@ -1,9 +1,59 @@
 <script setup>
-
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <main>
-    
-  </main>
+  <form>
+    <div>
+      <label for="theme">Sélectionner le thème à utiliser :</label>
+      <select name="theme" id="theme">
+        <option value="colors">Couleurs</option>
+        <option value="abstract">Formes abstraites</option>
+        <option value="cards">Cartes à jouer</option>
+      </select>
+    </div>
+    <div>
+      <label for="mode">Sélectionner la variante :</label>
+      <select name="mode" id="mode">
+        <option value="memory">Test de mémoire</option>
+        <option value="relearning">Réapprentissage</option>
+      </select>
+    </div>
+    <div>
+      <label for="name">Saisissez votre prénom et votre nom :</label>
+      <input type="text" id="name" required>
+    </div>
+    <div class="validation">
+      <button type="button" disabled>Démarrer</button>
+    </div>
+  </form>
 </template>
+
+<style scoped>
+  form{
+    width: 40%;
+    margin: 15px;
+    font-size: 2rem;
+  }
+  div{
+    display: flex;
+    flex-direction: column;
+    margin: 5px 0;
+  }
+  .validation{
+    border-top: 1px solid var(--color2);
+  }
+  label{
+    margin: 5px 0;
+  }
+  select, input{
+    width: 50%;
+    padding: 5px;
+  }
+  button{
+    background-color: var(--color3);
+    border: none;
+    padding: 10px;
+    margin: 5px 0;
+  }
+</style>
